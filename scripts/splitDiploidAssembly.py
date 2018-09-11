2b95f0d4b25c379163a2c306c61e5ae02cc78c9f
pippel <pippel@mpi-cbg.de>
2018-09-11 15:52:54 +0200

    bugfix access python dict only is creads are available

--- scripts/splitDiploidAssembly.py
+++ scripts/splitDiploidAssembly.py
@@ -67,29 +67,28 @@ for fn in fIn:
 
         path = args["path"]
         preads = args["preads"]
-        rreads = args["rreads"]
-        creads = args["creads"]
+        rreads = args["rreads"] 
         length = int(args["length"][0])
 
         if e1 == e2 and e1 != -1:
           fout_a.write(">{} path={} ends={} length={} preads={} rreads={}".format(name, path[0], ",".join(ends), length, ",".join(rreads), ",".join(rreads)))
-          if creads:
-              fout_a.write(" creads={}".format(",".join(creads)))
+          if 'creads' in args:
+            fout_a.write(" creads={}".format(",".join(args["creads"])))
           fout_a.write("\n{}\n".format(wrap_seq(seq, 100)))
           if (length > 500000):
-              eprint("WARNING Found very long alternative Contig (bubble): {} lenght: {}.".format(name, length))
+            eprint("WARNING Found very long alternative Contig (bubble): {} lenght: {}.".format(name, length))
           continue
 
         if (e1 != -1 or e2 != -1) and length < 100000:
           fout_a.write(">{} path={} ends={} length={} preads={} rreads={}".format(name, path[0], ",".join(ends), length, ",".join(preads), ",".join(rreads)))
-          if creads:
-            fout_a.write(" creads={}".format(",".join(creads)))
+          if 'creads' in args:
+            fout_a.write(" creads={}".format(",".join(args["creads"])))
           fout_a.write("\n{}\n".format(wrap_seq(seq, 100)))
           continue
 
         fout_p.write(">{} path={} ends={} length={} preads={} rreads={}".format(name, path[0], ",".join(ends), length, ",".join(preads), ",".join(rreads)))
-        if creads:
-            fout_p.write(" creads={}".format(",".join(creads)))
+        if 'creads' in args:
+            fout_p.write(" creads={}".format(",".join(args["creads"])))
         fout_p.write("\n{}\n".format(wrap_seq(seq, 100)))
     f.close()
     
