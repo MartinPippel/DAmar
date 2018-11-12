bba0126bb676d4533ec782b2432c60c57cc5e3c2
pippel <pippel@mpi-cbg.de>
2018-11-12 15:31:13 +0100

    purgeHaplotigs add step4 merge bam files

--- scripts/createPurgeHaplotigPlans.sh
+++ scripts/createPurgeHaplotigPlans.sh
@@ -105,10 +105,48 @@ then
    		do
         	name=$(basename ${x%.subreads.fa.gz})
         	    		
-    	echo "minimap2 -a -x map-pb -t ${CT_PURGEHAPLOTIGS_MINIMAP2ALNTHREADS} ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/${ref}.idx ${x} | samtools view -hF 256 - | samtools sort -@ ${CT_PURGEHAPLOTIGS_SAMTOOLSTHREADS} -m ${CT_PURGEHAPLOTIGS_SAMTOOLSMEM}G -o ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/${ref}_${name}_minmap2.sort.bam -T /tmp/${ref}_${name}_minmap2.sort.tmp | samtools index ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/${ref}_${name}_minmap2.sort.bam"        	
+    	echo "minimap2 -a -x map-pb -t ${CT_PURGEHAPLOTIGS_MINIMAP2ALNTHREADS} ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/${ref}.idx ${x} | samtools view -hF 256 - | samtools sort -@ ${CT_PURGEHAPLOTIGS_SAMTOOLSTHREADS} -m ${CT_PURGEHAPLOTIGS_SAMTOOLSMEM}G -o ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/${ref}_${name}_minmap2.sort.bam -T /tmp/${ref}_${name}_minimap2.sort.tmp | samtools index -@ ${CT_PURGEHAPLOTIGS_SAMTOOLSTHREADS} ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/${ref}_${name}_minimap2.sort.bam"        	
 		done > purgeHaplotigs_03_minimap2_block_${FIX_DB}.${slurmID}.plan 
     	echo "minimap2 $(${PURGEHAPLOTIGS_ENV} && minimap2 --version && ${PURGEHAPLOTIGS_ENV_DEACT})" > purgeHaplotigs_03_minimap2_block_${FIX_DB}.${slurmID}.version
-		echo "samtools $(${PURGEHAPLOTIGS_ENV} && samtools 2>&1 | grep Version | awk '{print $2}' && ${PURGEHAPLOTIGS_ENV_DEACT})" >> purgeHaplotigs_03_minimap2_block_${FIX_DB}.${slurmID}.version		
+		echo "samtools $(${PURGEHAPLOTIGS_ENV} && samtools 2>&1 | grep Version | awk '{print $2}' && ${PURGEHAPLOTIGS_ENV_DEACT})" >> purgeHaplotigs_03_minimap2_block_${FIX_DB}.${slurmID}.version
+	### 4-bamMerge
+    elif [[ ${currentStep} -eq 4 ]]
+    then
+        ### clean up plans 
+        for x in $(ls purgeHaplotigs_04_*_*_${FIX_DB}.${slurmID}.* 2> /dev/null)
+        do            
+            rm $x
+        done
+        
+        if [[ ! -d ${CT_PURGEHAPLOTIGS_PACBIOFASTA} ]]
+        then
+        	(>&2 echo "ERROR - Variable ${CT_PURGEHAPLOTIGS_PACBIOFASTA} is not set or cannot be accessed")
+        	exit 1
+        fi
+        
+        ref=$(basename ${CT_PURGEHAPLOTIGS_INFASTA%.fasta})
+		
+		if [[ ! -f ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/${ref}.idx ]]
+		then 
+			(>&2 echo "ERROR - file ${ref}.idx not available. Create an reference index first!")
+			exit 1
+		fi
+        
+                
+    	# create input bam file list 
+   		for x in ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/${ref}_*_minmap2.sort.bam   		
+   		do
+   			if [[ ! -f ${x} || ! -s ${x} ]]
+   			then
+   				(>&2 echo "WARNING - file ${x} not available or empty")
+   			else
+   				echo "${x}" 
+   			fi      						
+		done > ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/bamlist.fofn
+        	    		
+    	echo "samtools merge -b ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/bamlist.fofn -@ ${CT_PURGEHAPLOTIGS_SAMTOOLSTHREADS} ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/${ref}_minimap2.sort.bam" > purgeHaplotigs_04_bamMerge_single_${FIX_DB}.${slurmID}.plan
+    	echo "samtools index -@ ${CT_PURGEHAPLOTIGS_SAMTOOLSTHREADS} ${CT_PURGEHAPLOTIGS_OUTDIR}/purgeHaplotigs_${CT_PURGEHAPLOTIGS_RUNID}/${ref}_minimap2.sort.bam" >> purgeHaplotigs_04_bamMerge_single_${FIX_DB}.${slurmID}.plan
+   		echo "samtools $(${PURGEHAPLOTIGS_ENV} && samtools 2>&1 | grep Version | awk '{print $2}' && ${PURGEHAPLOTIGS_ENV_DEACT})" > purgeHaplotigs_04_bamMerge_single_${FIX_DB}.${slurmID}.version				
     else
         (>&2 echo "step ${currentStep} in CT_PURGEHAPLOTIGS_TYPE ${CT_PURGEHAPLOTIGS_TYPE} not supported")
         (>&2 echo "valid steps are: ${myTypes[${CT_PURGEHAPLOTIGS_TYPE}]}")
