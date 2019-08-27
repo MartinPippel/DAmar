9a41432c7b0024d116ca52cfd4215857e9a0f551
pippel <pippel@mpi-cbg.de>
2019-08-27 14:01:34 +0200

    bufgix step 11 repeat masking pipeline

--- scripts/createRepmaskPlans.sh
+++ scripts/createRepmaskPlans.sh
@@ -520,7 +520,7 @@ then
                 NUMACTL=""
             fi
 
-            echo -n "cd ${RAW_REPAMSK_OUTDIR} && PATH=${DAZZLER_PATH}/bin:\${PATH} ${NUMACTL}${DAZZLER_PATH}/bin/daligner${REPMASK_DALIGNER_OPT} ${REP} ${RAW_DAZZ_DB%.db}.@${x}"
+            echo -n "cd ${RAW_REPAMSK_OUTDIR} && PATH=${DAZZLER_PATH}/bin:\${PATH} ${NUMACTL}${DAZZLER_PATH}/bin/daligner${REPMASK_DALIGNER_OPT} ${REP} ${RAW_DAZZ_DB%.db}.${x} ${RAW_DAZZ_DB%.db}.@${x}"
             for y in $(seq ${x} $((${x}+${n}-1)))
             do
                 if [[ ${y} -gt ${nblocks} ]]
